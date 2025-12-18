3d2deb704af01ad2525894ce91ca76bd3763eb31
rutwikg <63323403+rutwikg@users.noreply.github.com>
2025-12-18 23:43:55 +0100

    Add copyright and author details to gradLoss.py
    Added copyright notice and author information to gradLoss.py

--- Losses/gradLoss.py
+++ Losses/gradLoss.py
@@ -1,4 +1,19 @@
 # Author - Rutwik Gulakala - gulakala@iam.rwth-aachen.de
+
+# =============================================================================
+# Copyright (c) 2025 Rutwik Gulakala
+# All Rights Reserved.
+#
+# This source code is part of an ongoing research project and is made
+# publicly visible for academic transparency and peer review purposes only.
+#
+# Unauthorized copying, modification, distribution, or use of this code,
+# in whole or in part, is strictly prohibited without prior written
+# permission from the author.
+#
+# This code is provided "AS IS", without warranty of any kind.
+# =============================================================================
+
 # Grad-loss for physics-driven training from DG3Net Paper
 
 # -------------------------
